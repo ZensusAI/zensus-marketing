3429ca20764306f8c53cc83b446caef54b129224
Ajin Sunny <ajinsunny@users.noreply.github.com>
2026-04-17 14:16:43 +0530

    chore: wrap app in HelmetProvider for per-route meta

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Pricing from "./pages/Pricing";
@@ -13,25 +14,27 @@ import RedirectToApp from "./components/RedirectToApp";
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/forecast" element={<Forecast />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/privacy" element={<RedirectToApp />} />
-          <Route path="/support" element={<RedirectToApp />} />
-          <Route path="/features" element={<RedirectToApp />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <HelmetProvider>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/forecast" element={<Forecast />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/privacy" element={<RedirectToApp />} />
+            <Route path="/support" element={<RedirectToApp />} />
+            <Route path="/features" element={<RedirectToApp />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  </HelmetProvider>
 );
 
 export default App;
